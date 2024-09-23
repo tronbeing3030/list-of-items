54f89c3e5683ad1859341bac0c88801d429ca634
tronbeing3030 <kaiwarharsha@gmail.com>
2024-09-23 17:53:35 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,19 @@
+const inputBox = document.getElementById('input-box');
+const listContainer = document.getElementById('list-container');
+
+function newlist() {
+    if (inputBox.value === "") {
+    }
+    else {
+        let li = document.createElement('li');
+        li.innerHTML = inputBox.value;
+        li.style.fontSize = "1.1rem";
+        listContainer.appendChild(li);
+    }
+    inputBox.value = "";
+};
+
+function del() {
+    listContainer.removeChild(listContainer.lastChild);
+    // alert("Working!!!");
+}
